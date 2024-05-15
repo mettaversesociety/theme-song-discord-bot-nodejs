486bf24649c8e0329a5aa6bcdcb2258dc1439d20
Edwin Lin <edwinlin@Edwins-MacBook-Air.local>
2024-05-15 17:03:56 -0400

    getvoiceconnectionstatus

--- bot.js
+++ bot.js
@@ -8,6 +8,7 @@ const {
   createAudioPlayer,
   createAudioResource,
   AudioPlayerStatus,
+  getVoiceConnection,
   VoiceConnectionStatus,
   entersState, // Add this import
   VoiceConnectionDisconnectReason,
